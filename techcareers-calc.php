1b3176303bac1156fcf0c3e37a365f7a1bcba020
Erpreet <preetkaurchd15@gmail.com>
2020-10-07 14:37:23 -0600

    techcareers-calc-php

---
+++ techcareers-calc.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name:       TECHCareers Calculator
+ * Plugin URI:        https://TECHCareers.ca
+ * Description:       A simple PHP calculator
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:           Preet
+ * Author URI:        
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ 
+ */
